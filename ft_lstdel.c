262a84aa4fab2937713eb04eeb7ec5f51f211ad3
Niklas Pimenoff <pimenoff.niklas@gmail.com>
2020-11-02 18:55:48 +0200

    lstdel now checks if the functionpointer is valid

--- ft_lstdel.c
+++ ft_lstdel.c
@@ -6,7 +6,7 @@
 /*   By: npimenof <npimenof@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/22 09:36:43 by npimenof          #+#    #+#             */
-/*   Updated: 2019/10/24 10:36:14 by npimenof         ###   ########.fr       */
+/*   Updated: 2020/11/02 15:39:09 by npimenof         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,8 @@ void	ft_lstdel(t_list **alst, void (*del)(void *, size_t))
 		{
 			tmp = *alst;
 			*alst = (*alst)->next;
-			del(tmp->content, tmp->content_size);
+			if (del)
+				del(tmp->content, tmp->content_size);
 			free(tmp);
 			tmp = NULL;
 		}
