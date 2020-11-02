46f143cfdc82011e4aaa3de63b3cb628a64c279d
Niklas Pimenoff <pimenoff.niklas@gmail.com>
2020-11-02 18:55:10 +0200

    lstclone wont reallocate data now.. might not work on some earlier programs, but this prevents leaks and makes freeing easier

--- ft_lstclone.c
+++ ft_lstclone.c
@@ -6,19 +6,19 @@
 /*   By: npimenof <npimenof@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/21 11:22:51 by npimenof          #+#    #+#             */
-/*   Updated: 2020/10/21 11:26:46 by npimenof         ###   ########.fr       */
+/*   Updated: 2020/11/02 18:31:04 by npimenof         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list	*ft_lstclone(t_list *list, size_t size)
+t_list	*ft_lstclone(t_list *list)
 {
 	t_list	*result;
 
 	if (list == NULL)
 		return (NULL);
-	result = ft_lstnew(list->content, size);
-	result->next = ft_lstclone(list->next, size);
+	result = ft_lstcontent(list->content);
+	result->next = ft_lstclone(list->next);
 	return (result);
 }
