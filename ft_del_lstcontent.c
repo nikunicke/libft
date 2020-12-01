3a228dfe46bbe1d99dc572dca54deddd0295aa86
nikunicke <pimenoff.niklas@gmail.com>
2020-12-01 16:50:47 +0000

    fixed for linux

--- ft_del_lstcontent.c
+++ ft_del_lstcontent.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_del_lstcontent.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: npimenof <npimenof@student.42.fr>          +#+  +:+       +#+        */
+/*   By: npimenof <npimenof@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/26 10:50:51 by npimenof          #+#    #+#             */
-/*   Updated: 2020/02/27 18:05:58 by npimenof         ###   ########.fr       */
+/*   Updated: 2020/12/01 16:50:25 by npimenof         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,5 @@ void	ft_del_lstcontent(void *content, size_t content_size)
 {
 	free(content);
 	content = NULL;
-	content_size = 0;
+	content_size = content_size - content_size;
 }
--- ft_memchr.c
+++ ft_memchr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: npimenof <npimenof@student.42.fr>          +#+  +:+       +#+        */
+/*   By: npimenof <npimenof@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/17 15:36:27 by npimenof          #+#    #+#             */
-/*   Updated: 2019/10/22 16:21:09 by npimenof         ###   ########.fr       */
+/*   Updated: 2020/12/01 16:50:16 by npimenof         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	while (n--)
 	{
 		if (a[i] == (unsigned char)c)
-			return ((void *)&s[i]);
+			return ((void *)s + i);
 		i++;
 	}
 	return (NULL);
